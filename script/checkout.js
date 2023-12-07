a9bbbfb41df8085981ed957973ae558d031dde7c
D-Raff <damonraffels@gmail.com>
2023-12-07 19:24:59 +0200

    fixing spinner

--- script/checkout.js
+++ script/checkout.js
@@ -1,6 +1,6 @@
 let cart = JSON.parse(localStorage.getItem('cart'))
 
-let cartItems = Object.groupBy(cart, item=> item.name)
+let cartItems = Object.groupBy(cart, item => item.name)
 
 let cartDisplay = document.querySelector("[data-cart-list]");
 
@@ -10,30 +10,27 @@ let cartTotal = document.querySelector("[data-total]");
 
 let totalPrice = 0
 
-function displaycart(){
-    cartDisplay.innerHTML = ''
-    if(cartItems){
-        for(let i in cartItems){
-          let itemPrices = cartItems[i][0].price * cartItems[i].length
-            cartDisplay.innerHTML += `
+function displaycart() {
+  cartDisplay.innerHTML = ''
+  if (cartItems) {
+    for (let i in cartItems) {
+      let itemPrices = cartItems[i][0].price * cartItems[i].length
+      cartDisplay.innerHTML += `
               <tr class="prod-row">
-                <td class="w-25" id="tableSpacing"><img src="${cartItems[i][0].img}" data-cart-img></td>
-                <td id="tableSpacing">${cartItems[i][0].name}</td>
-                <td id="tableSpacing">${cartItems[i][0].description}</td>
-                <td id="tableSpacing">${cartItems[i].length}</td>
-                <td id="tableSpacing">R${itemPrices}</td>
+                <td class="w-25"><img src="${cartItems[i][0].img}" data-cart-img></td>
+                <td class="w-25">${cartItems[i][0].name}</td>
+                <td class="w-25">${cartItems[i][0].description}</td>
+                <td class="w-25">${cartItems[i].length}</td>
+                <td class="w-25">R${itemPrices}</td>
               </tr>
             `
-            totalPrice += +cartItems[i][0].price * cartItems[i].length;
-          }
-          cartTotal.value = totalPrice
-    }else{
-    noItems.innerHTML = `<div class="text-center">
-    <div class="spinner-border" role="status">
-      <span class="sr-only"></span>
-    </div>
-  </div>`
+      totalPrice += +cartItems[i][0].price * cartItems[i].length;
     }
+    cartTotal.value = `R${totalPrice}`
+  } 
+  else {
+    noItems.innerHTML = `why wont this kak work`
+  }
 }
 displaycart()
 
@@ -42,5 +39,5 @@ let clearBtn = document.querySelector('[data-clear]')
 
 clearBtn.addEventListener('click', function () {
   localStorage.removeItem('cart')
-  displaycart()
+  location.reload()
 })
