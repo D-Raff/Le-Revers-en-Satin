34d74226f4f5db7fc5cd9b024ace81cece9ae5ad
D-Raff <damonraffels@gmail.com>
2023-12-08 17:28:17 +0200

    finished styling

--- script/code.js
+++ script/code.js
@@ -2,3 +2,19 @@
 
 const date = document.querySelector('[data-date]')
 date.textContent = new Date().getFullYear()
+
+// timeout for form in contact page
+let submitBtn = document.querySelector('[data-submit]')
+let email = document.querySelector('[data-mail]')
+let message = document.querySelector('[data-message]')
+
+function emptyForm(){
+    email.value = ''
+    message.value = ''
+}
+
+function clearForm(){
+    setTimeout(emptyForm ,1000)
+}
+
+submitBtn.addEventListener('click', clearForm)
